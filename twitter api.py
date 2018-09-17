67f1db1e92a2ae3ad7be22d256b52d3d1b651de6
Gang Wei <43114190+WayGang@users.noreply.github.com>
2018-09-17 19:14:46 -0400

    first part finished

--- twitter api.py
+++ twitter api.py
@@ -1,6 +1,6 @@
 import tweepy
 from tweepy import OAuthHandler
-import json
+
 
 consumer_key = 'bvay7XKTdnuVg2TawE3OvY89D'
 consumer_secret = '8tDJygIpj9nkmpBUENlFEG5Cxkgny9D2MOISVcdAYfOutNQ9JI'
@@ -8,22 +8,27 @@ access_token = '1040111094516404224-Wdo2WoRvvtvnEXVTD7GdqL8J8GJb6y'
 access_secret = 'WV0VlSbnpIBNmGTR9nYVcsTAleB4mo48VfqplHayjcJPn'
 
 
-@classmethod
-def parse(cls, api, raw):
-    status = cls.first_parse(api, raw)
-    setattr(status, 'json', json.dumps(raw))
-    return status
-
-
-# Status() is the data model for a tweet
-tweepy.models.Status.first_parse = tweepy.models.Status.parse
-tweepy.models.Status.parse = parse
-# User() is the data model for a user profil
-tweepy.models.User.first_parse = tweepy.models.User.parse
-tweepy.models.User.parse = parse
-# You need to do it for all the models you need
-
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
+
+def getTweets(screen_name):
+    tweets = api.user_timeline(screen_name=screen_name,count=40,include_rts=False, exclude_replies=True)
+    imageList = []
+    i = 0
+    for status in tweets:
+        image = status.entities.get('media', [])
+        if(len(image) > 0):
+            image_url = status.entities['media'][0]['media_url']
+            file_name = "%03d.jpg"%i
+            urlretrieve(image_url, file_name)
+            imageList.append(file_name)
+            i = i + 1
+    return imageList
+
+
+
+
+
+
