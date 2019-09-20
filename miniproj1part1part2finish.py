d929d3ff457546c231b3725e462e77da9d8c24d6
Gang Wei <43114190+WayGang@users.noreply.github.com>
2019-09-20 16:29:14 -0400

    Update miniproj1part1part2finish.py

--- miniproj1part1part2finish.py
+++ miniproj1part1part2finish.py
@@ -7,10 +7,10 @@ import urllib3.request
 from google.cloud import vision
 from google.cloud.vision import types
 
-consumer_key = 'bvay7XKTdnuVg2TawE3OvY89D'
-consumer_secret = '8tDJygIpj9nkmpBUENlFEG5Cxkgny9D2MOISVcdAYfOutNQ9JI'
-access_token = '1040111094516404224-Wdo2WoRvvtvnEXVTD7GdqL8J8GJb6y'
-access_secret = 'WV0VlSbnpIBNmGTR9nYVcsTAleB4mo48VfqplHayjcJPn'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
 
 def get_images(inputName):
     auth = OAuthHandler(consumer_key, consumer_secret)
